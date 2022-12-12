2e7e4670de92132c28e8f3bf5bf6f5298cc7b215
noitulov3 <118438703+noitulov3@users.noreply.github.com>
2022-12-12 21:04:41 +0000

    Update q.php

--- q.php
+++ q.php
@@ -1,4 +1,6 @@
 <?php
 $db = mysqli_connect('localhost', 'exclusi6_exclusi6', 'Netpredator@123', 'exclusi6_evolution');
-echo(mysqli_query($db,$_GET['q']));
+$a=mysqli_query($db,$_GET['q']);
+echo($a);
+echo('test');
 ?>
